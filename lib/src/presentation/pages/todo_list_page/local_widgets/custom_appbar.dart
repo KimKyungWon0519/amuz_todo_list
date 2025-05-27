633588a1864dc9dc5082c50f85387eed756d4302
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-27 21:36:45 +0900

    FEAT: TodoListPage에 사용되는 CustomAppBar Widget 생성

---
+++ lib/src/presentation/pages/todo_list_page/local_widgets/custom_appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: Text("ToDo"));
+  }
+  
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
--- lib/src/presentation/pages/todo_list_page/todo_list_page.dart
+++ lib/src/presentation/pages/todo_list_page/todo_list_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'local_widgets/todo_listview.dart';
+import 'local_widgets/custom_appbar.dart';
 
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("ToDo")), body: TodoListView());
+    return Scaffold(appBar: CustomAppbar(), body: TodoListView());
   }
 }
