e87dc7c2ada825ac6970ec5dd4ecb6c21807677f
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-27 21:28:26 +0900

    FEAT: Todo ListView 위젯 생성

---
+++ lib/src/presentation/pages/todo_list_page/local_widgets/todo_listview.dart
@@ -0,0 +1,14 @@
+import 'package:amuz_todo_list/src/presentation/pages/todo_list_page/local_widgets/todo_tile.dart';
+import 'package:flutter/material.dart';
+
+class TodoListView extends StatelessWidget {
+  const TodoListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) => TodoTile(),
+      itemCount: 50,
+    );
+  }
+}
--- lib/src/presentation/pages/todo_list_page/todo_list_page.dart
+++ lib/src/presentation/pages/todo_list_page/todo_list_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'local_widgets/todo_tile.dart';
+import 'local_widgets/todo_listview.dart';
 
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("ToDo")), body: TodoTile());
+    return Scaffold(appBar: AppBar(title: Text("ToDo")), body: TodoListView());
   }
 }
