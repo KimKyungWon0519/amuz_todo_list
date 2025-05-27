8d9f4d1712ddb956c699badce213eb13406aa7ca
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-27 21:26:48 +0900

    FEAT: Todo Tile 생성

---
+++ lib/src/presentation/pages/todo_list_page/local_widgets/todo_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoTile extends StatelessWidget {
+  const TodoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("Todo Name"),
+      trailing: IconButton(onPressed: null, icon: Icon(Icons.delete)),
+    );
+  }
+}
