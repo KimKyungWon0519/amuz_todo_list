ad82e8cccffa9994c2ece9e82cdf4b3ac2fc1791
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-27 21:11:21 +0900

    FEAT: Todo 목록 페이지 생성

--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -1,7 +1,13 @@
+import 'package:amuz_todo_list/src/presentation/pages/todo_list_page/todo_list_page.dart';
 import 'package:go_router/go_router.dart';
 
 final class AppRoutes {
   const AppRoutes._();
 
-  static final List<RouteBase> routes = [];
+  static final GoRoute main = GoRoute(
+    path: '/',
+    builder: (context, state) => TodoListPage(),
+  );
+
+  static final List<RouteBase> routes = [main];
 }
