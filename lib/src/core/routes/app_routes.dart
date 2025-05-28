99b16a19af9f3a1015610140b630a8e80d5c27d2
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-28 19:57:51 +0900

    FEAT: UserPage 생성

--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:amuz_todo_list/src/presentation/pages/details_todo_page/details_todo_page.dart';
 import 'package:amuz_todo_list/src/presentation/pages/todo_list_page/todo_list_page.dart';
+import 'package:amuz_todo_list/src/presentation/pages/user_page/user_page.dart';
 import 'package:go_router/go_router.dart';
 
 final class AppRoutes {
@@ -15,5 +16,10 @@ final class AppRoutes {
     builder: (context, state) => DetailsTodoPage(),
   );
 
-  static final List<RouteBase> routes = [main, detailsTodo];
+  static final GoRoute userPage = GoRoute(
+    path: '/user',
+    builder: (context, state) => UserPage(),
+  );
+
+  static final List<RouteBase> routes = [main, detailsTodo, userPage];
 }
