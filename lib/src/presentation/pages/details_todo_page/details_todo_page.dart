e79c4fd0717aa4b17c171ec0990a9a088240ee71
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-28 17:26:56 +0900

    FEAT: TodoTitle Widget 생성

--- lib/src/presentation/pages/details_todo_page/details_todo_page.dart
+++ lib/src/presentation/pages/details_todo_page/details_todo_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'local_widgets/todo_title.dart';
+
 class DetailsTodoPage extends StatelessWidget {
   const DetailsTodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Padding(padding: EdgeInsets.all(16))),
+      body: SafeArea(
+        child: Padding(padding: EdgeInsets.all(16), child: TodoTitle()),
+      ),
     );
   }
 }
