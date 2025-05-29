71bbe7271b4e24e27cba2021706440cf0773054f
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-29 17:38:55 +0900

    FEAT: Write Todo Page AppBar 추가

--- lib/src/presentation/pages/write_todo_page.dart/write_todo_page.dart
+++ lib/src/presentation/pages/write_todo_page.dart/write_todo_page.dart
@@ -5,6 +5,6 @@ class WriteTodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: Text("ToDo 추가")));
   }
 }
