ad78283a6b8eb3d92dfef3289d2c6372cba6d89b
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-30 02:26:56 +0900

    FEAT: TagListView 추가

--- lib/src/presentation/pages/write_todo_page.dart/write_todo_page.dart
+++ lib/src/presentation/pages/write_todo_page.dart/write_todo_page.dart
@@ -1,3 +1,4 @@
+import 'package:amuz_todo_list/src/presentation/pages/write_todo_page.dart/local_widgets/tag_listview.dart';
 import 'package:flutter/material.dart';
 
 import 'local_widgets/title_field.dart';
@@ -11,7 +12,16 @@ class WriteTodoPage extends StatelessWidget {
       appBar: AppBar(title: Text("ToDo 추가")),
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: Column(spacing: 8, children: [TitleField()]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 8,
+          children: [
+            TitleField(),
+            Divider(),
+            SizedBox(height: 50, child: TagListview()),
+            Divider(),
+          ],
+        ),
       ),
     );
   }
