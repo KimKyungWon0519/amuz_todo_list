d89b4a003375f836981ef8567b0d6f0598dd9298
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-30 18:54:43 +0900

    FEAT: Domain.Model에 Todo 생성

---
+++ lib/src/domain/model/todo.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo.freezed.dart';
+
+@freezed
+abstract class Todo with _$Todo {
+  const factory Todo({
+    required String name,
+    required Set<String> tags,
+    required List<String> images,
+    required DateTime createAt,
+    required DateTime? updateAt,
+  }) = _Todo;
+}
