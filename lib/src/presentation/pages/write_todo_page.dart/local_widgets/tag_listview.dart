5fc51e2edd9bd6dc5cb33cf90467c0dbf17e2eb9
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-31 03:52:19 +0900

    FEAT: TagListView에 DetailNotifier.Todo.tags의 watch 추가

--- lib/src/presentation/pages/write_todo_page.dart/local_widgets/tag_listview.dart
+++ lib/src/presentation/pages/write_todo_page.dart/local_widgets/tag_listview.dart
@@ -1,3 +1,5 @@
+import 'package:amuz_todo_list/src/domain/model/tag.dart' show Tag;
+import 'package:amuz_todo_list/src/presentation/riverpods/detail_todo_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,6 +8,10 @@ class TagListview extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final Set<Tag> tags = ref.watch(
+      detailTodoNotifierProvider.select((value) => value.tags),
+    );
+
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemBuilder:
