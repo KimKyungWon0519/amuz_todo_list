cab180d19ffe2856b4eae70ad5628852f68b3159
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-30 19:32:17 +0900

    FEAT: LocalDatabaseRepository.getAllTags 추가

--- lib/src/data/repositories/local_database_repository_impl.dart
+++ lib/src/data/repositories/local_database_repository_impl.dart
@@ -1,4 +1,7 @@
+import 'package:amuz_todo_list/src/data/data_sources/local_database.dart';
 import 'package:amuz_todo_list/src/data/data_sources/local_database_helper.dart';
+import 'package:amuz_todo_list/src/data/mapper/tag_mapper.dart';
+import 'package:amuz_todo_list/src/domain/model/tag.dart' as Domain;
 import 'package:amuz_todo_list/src/domain/repositories/local_database_repository.dart';
 
 class LocalDatabaseRepositoryImpl implements LocalDatabaseRepository {
@@ -6,4 +9,11 @@ class LocalDatabaseRepositoryImpl implements LocalDatabaseRepository {
 
   LocalDatabaseRepositoryImpl(LocalDatabaseHelper localDatabaseHelper)
     : _localDatabaseHelper = localDatabaseHelper;
+
+  @override
+  Future<Set<Domain.Tag>> getAllTags() async {
+    List<Tag> tags = await _localDatabaseHelper.getAllTags();
+
+    return tags.map((e) => e.toDomainModel()).toSet();
+  }
 }
--- lib/src/domain/repositories/local_database_repository.dart
+++ lib/src/domain/repositories/local_database_repository.dart
@@ -1,1 +1,5 @@
-abstract interface class LocalDatabaseRepository {}
+import 'package:amuz_todo_list/src/domain/model/tag.dart';
+
+abstract interface class LocalDatabaseRepository {
+  Future<Set<Tag>> getAllTags();
+}
