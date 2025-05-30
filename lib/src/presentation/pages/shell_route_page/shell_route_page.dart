e56b49992dfa0ada61b9c0fb1e5f6650ec8c8ca8
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-30 18:10:42 +0900

    FEAT: BottomNav에 Add FAB 추가

--- lib/src/presentation/pages/shell_route_page/shell_route_page.dart
+++ lib/src/presentation/pages/shell_route_page/shell_route_page.dart
@@ -1,3 +1,4 @@
+import 'package:amuz_todo_list/src/core/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -31,6 +32,11 @@ class ShellRoutePage extends HookWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.push(AppRoutes.writeTodo.path),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
