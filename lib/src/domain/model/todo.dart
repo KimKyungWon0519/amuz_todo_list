1d9e4a3f9da9c058a2b805278459db8a63b89f92
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-30 19:09:41 +0900

    FEAT: Domain.Todo.tags Type을 Set<Domain.Tag>로 변경

--- lib/src/domain/model/todo.dart
+++ lib/src/domain/model/todo.dart
@@ -1,3 +1,4 @@
+import 'package:amuz_todo_list/src/domain/model/tag.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'todo.freezed.dart';
@@ -7,7 +8,7 @@ abstract class Todo with _$Todo {
   const factory Todo({
     required int? id,
     required String title,
-    required Set<String> tags,
+    required Set<Tag> tags,
     required List<String> images,
     required DateTime createAt,
     required DateTime? updateAt,
