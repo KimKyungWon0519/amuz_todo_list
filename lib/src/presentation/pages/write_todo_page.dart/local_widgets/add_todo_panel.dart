caf35cf9180fd06c27254b07603e73fd0a6e7b6b
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-31 14:32:29 +0900

    FEAT: 태그 관리 패널에서 확장 타일 추가

--- lib/src/presentation/pages/write_todo_page.dart/local_widgets/add_todo_panel.dart
+++ lib/src/presentation/pages/write_todo_page.dart/local_widgets/add_todo_panel.dart
@@ -6,8 +6,27 @@ class AddTodoPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(mainAxisSize: MainAxisSize.max, children: [
-          
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          ExpansionPanelList(
+            children: [
+              ExpansionPanel(
+                headerBuilder:
+                    (context, isExpanded) => ListTile(title: Text('추가된 태그')),
+                body: Wrap(),
+              ),
+            ],
+          ),
+          ExpansionPanelList(
+            children: [
+              ExpansionPanel(
+                headerBuilder:
+                    (context, isExpanded) => ListTile(title: Text('모든 태그')),
+                body: Wrap(),
+              ),
+            ],
+          ),
         ],
       ),
     );
