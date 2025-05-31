517fa60467fa1b892ea3db1e844676fb61048971
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-31 18:09:28 +0900

    FEAT: AddTagPanel 패키지 추가

--- lib/src/presentation/pages/write_todo_page.dart/local_widgets/tag_tile.dart
+++ lib/src/presentation/pages/write_todo_page.dart/local_widgets/tag_tile.dart
@@ -1,4 +1,4 @@
-import 'package:amuz_todo_list/src/presentation/pages/write_todo_page.dart/local_widgets/add_tag_panel.dart';
+import 'package:amuz_todo_list/src/presentation/pages/add_tag_panel/add_tag_panel.dart';
 import 'package:amuz_todo_list/src/presentation/riverpods/detail_todo_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
