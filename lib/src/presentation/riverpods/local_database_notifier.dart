86a22fc859ee1f9338c3d1f03a3832982db0bb26
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-31 15:17:58 +0900

    FEAT: 모든 Tag를 가져오는 Notifier 추가

---
+++ lib/src/presentation/riverpods/local_database_notifier.dart
@@ -0,0 +1,15 @@
+import 'package:amuz_todo_list/src/domain/model/tag.dart';
+import 'package:amuz_todo_list/src/domain/repositories/local_database_repository.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'local_database_notifier.g.dart';
+
+@Riverpod(keepAlive: false)
+Future<Set<Tag>> getAllTags(Ref ref) async {
+  final LocalDatabaseRepository repository = GetIt.I<LocalDatabaseRepository>();
+  final Set<Tag> tags = await repository.getAllTags();
+
+  return tags;
+}
