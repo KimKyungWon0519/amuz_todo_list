21376115d680f52387396009eb0a9afe7dbae89b
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-31 14:01:27 +0900

    FEAT: Tag 개수가 반영

--- lib/src/presentation/pages/write_todo_page.dart/local_widgets/tag_tile.dart
+++ lib/src/presentation/pages/write_todo_page.dart/local_widgets/tag_tile.dart
@@ -1,4 +1,6 @@
+import 'package:amuz_todo_list/src/presentation/riverpods/detail_todo_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class TagTile extends StatelessWidget {
   const TagTile({super.key});
@@ -6,7 +8,19 @@ class TagTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(title: Text('태그'), trailing: Text('1개'), onTap: () {}),
+      child: ListTile(
+        title: Text('태그'),
+        trailing: Consumer(
+          builder: (context, ref, child) {
+            final int tagCount = ref.watch(
+              detailTodoNotifierProvider.select((value) => value.tags.length),
+            );
+
+            return Text('$tagCount 개');
+          },
+        ),
+        onTap: () {},
+      ),
     );
   }
 }
