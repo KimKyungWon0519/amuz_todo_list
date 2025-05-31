57bff3970dfd3e1d7505c55d9de4ea3f1704480b
KimKyungWon0519 <gwon080870@gamil.com>
2025-05-31 18:44:31 +0900

    FEAT: AddImageButtonRow에 DetailTodoNotifier.state.image 구독

--- lib/src/presentation/pages/write_todo_page.dart/local_widgets/add_image_buttons_row.dart
+++ lib/src/presentation/pages/write_todo_page.dart/local_widgets/add_image_buttons_row.dart
@@ -1,12 +1,19 @@
+import 'package:amuz_todo_list/src/presentation/riverpods/detail_todo_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:amuz_todo_list/src/domain/model/image.dart' as Domain;
 
 import 'add_image_button.dart';
 
-class AddImageButtonsRow extends StatelessWidget {
+class AddImageButtonsRow extends ConsumerWidget {
   const AddImageButtonsRow({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Domain.Image> images = ref.watch(
+      detailTodoNotifierProvider.select((value) => value.images),
+    );
+
     return LayoutBuilder(
       builder: (context, constraints) {
         double itemSize = constraints.maxWidth / 5.5;
@@ -15,7 +22,7 @@ class AddImageButtonsRow extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           spacing: 8,
           children: List.generate(
-            5,
+            images.length + images.length < 5 ? 1 : 0,
             (_) => AddImageButton(size: Size(itemSize, itemSize)),
           ),
         );
