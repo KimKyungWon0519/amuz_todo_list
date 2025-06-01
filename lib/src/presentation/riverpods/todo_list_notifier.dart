31abab23d527a26c0e09b6a7e0125532400bccc3
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 01:57:08 +0900

    FEAT: TodoListNotifier의 초기 구성 구현

---
+++ lib/src/presentation/riverpods/todo_list_notifier.dart
@@ -0,0 +1,9 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'todo_list_notifier.g.dart';
+
+@riverpod
+class TodoListNotifier extends _$TodoListNotifier {
+  @override
+  void build() {}
+}
