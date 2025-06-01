b61d9580d3cf5865586205a39710d66ce90f8e64
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 03:23:47 +0900

    FEAT: Todo 목록을 반영

--- lib/src/presentation/pages/todo_list_page/local_widgets/todo_listview.dart
+++ lib/src/presentation/pages/todo_list_page/local_widgets/todo_listview.dart
@@ -1,15 +1,26 @@
 import 'package:amuz_todo_list/src/presentation/pages/todo_list_page/local_widgets/todo_tile.dart';
+import 'package:amuz_todo_list/src/presentation/riverpods/todo_list_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class TodoListView extends StatelessWidget {
+class TodoListView extends ConsumerWidget {
   const TodoListView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder:
-          (context, index) => Dismissible(key: UniqueKey(), child: TodoTile()),
-      itemCount: 50,
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue todos = ref.watch(todoListNotifierProvider);
+
+    return todos.when(
+      data: (todos) {
+        return ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return TodoTile();
+          },
+        );
+      },
+      error: (_, __) => Center(child: Text('오류가 발생하여 목록을 불러올 수 없습니다.')),
+      loading: () => const Center(child: CircularProgressIndicator()),
     );
   }
 }
