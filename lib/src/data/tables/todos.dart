0ca501e712376e33114f2763d7dd63d2ee2eab91
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 01:56:46 +0900

    FEAT: Todos Table에 isDone (완료) 속성 부여

--- lib/src/data/tables/todos.dart
+++ lib/src/data/tables/todos.dart
@@ -4,6 +4,7 @@ import 'package:drift/drift.dart';
 class Todos extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get title => text().withLength(min: 0, max: todoTitleMaxLength)();
+  BoolColumn get isDone => boolean().withDefault(const Constant(false))();
   DateTimeColumn get createDate => dateTime()();
   DateTimeColumn get updateDate => dateTime().nullable()();
 }
