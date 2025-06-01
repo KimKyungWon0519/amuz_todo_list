23c99a95ad2bda3ca601fdaa800b51887fd9374c
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-01 19:58:24 +0900

    FEAT: 제목 없이 저장 버튼을 누를 시 저장 실패 표시

--- lib/src/presentation/pages/write_todo_page.dart/local_widgets/save_button.dart
+++ lib/src/presentation/pages/write_todo_page.dart/local_widgets/save_button.dart
@@ -1,10 +1,27 @@
+import 'package:amuz_todo_list/src/presentation/riverpods/write_todo_notifier.dart';
+import 'package:amuz_todo_list/src/domain/model/todo.dart';
+import 'package:amuz_todo_list/src/presentation/widgets/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SaveButton extends StatelessWidget {
+class SaveButton extends ConsumerWidget {
   const SaveButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return TextButton(onPressed: null, child: Text('저장'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return TextButton(
+      onPressed: () => _onPressed(context, ref),
+      child: Text('저장'),
+    );
+  }
+
+  void _onPressed(BuildContext context, WidgetRef ref) {
+    Todo todo = ref.read(writeTodoNotifierProvider).todo;
+
+    if (todo.title.trim().isEmpty) {
+      showErrorDialog(context, title: '저장 실패', content: '제목을 입력해주세요.');
+
+      return;
+    }
   }
 }
