567ff78fe6ff11bf695e585555ba1c4a01ae10d5
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 03:19:20 +0900

    FEAT: TodoListNotifier.state를 Stream<List<Todo>>로 지정

--- lib/src/presentation/riverpods/todo_list_notifier.dart
+++ lib/src/presentation/riverpods/todo_list_notifier.dart
@@ -1,9 +1,19 @@
+import 'package:amuz_todo_list/src/domain/model/todo.dart';
+import 'package:amuz_todo_list/src/domain/repositories/local_database_repository.dart';
+import 'package:get_it/get_it.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'todo_list_notifier.g.dart';
 
 @riverpod
 class TodoListNotifier extends _$TodoListNotifier {
+  late final LocalDatabaseRepository _localDatabaseRepository;
+
+  TodoListNotifier()
+    : _localDatabaseRepository = GetIt.I<LocalDatabaseRepository>();
+
   @override
-  void build() {}
+  Stream<List<Todo>> build() {
+    return _localDatabaseRepository.watchAllTodos();
+  }
 }
