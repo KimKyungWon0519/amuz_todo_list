ade95cc847fb8aa78314600b8367ac6da7c1efd7
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 16:03:43 +0900

    FEAT: 완료 상태에 따른 Title 스타일 변경

--- lib/src/presentation/pages/details_todo_page/local_widgets/todo_title.dart
+++ lib/src/presentation/pages/details_todo_page/local_widgets/todo_title.dart
@@ -1,3 +1,4 @@
+import 'package:amuz_todo_list/src/domain/model/todo.dart';
 import 'package:amuz_todo_list/src/presentation/riverpods/details_todo_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -7,10 +8,13 @@ class TodoTitle extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    String title = ref.watch(
-      detailsTodoNotifierProvider.select((value) => value.title),
-    );
+    Todo todo = ref.watch(detailsTodoNotifierProvider);
 
-    return Text(title, style: Theme.of(context).textTheme.headlineMedium);
+    return Text(
+      todo.title,
+      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+        decoration: todo.isDone ? TextDecoration.lineThrough : null,
+      ),
+    );
   }
 }
