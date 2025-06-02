189cfc2877803f943fa8f87a5e07dfcb6987abbb
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 14:53:01 +0900

    FEAT: DetailsTodoNotifier 기본 구조 생성

---
+++ lib/src/presentation/riverpods/details_todo_notifier.dart
@@ -0,0 +1,8 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'details_todo_notifier.g.dart';
+
+@Riverpod(keepAlive: false)
+class DetailsTodoNotifier extends _$DetailsTodoNotifier {
+  void build() {}
+}
