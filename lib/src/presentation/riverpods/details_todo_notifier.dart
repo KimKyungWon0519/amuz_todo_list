1b16f38d737f3bf8f1267137ff9184e265cc19b6
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 15:03:34 +0900

    FEAT: DetailsTodoNotifier State를 Domain.Todo로 지정

--- lib/src/presentation/riverpods/details_todo_notifier.dart
+++ lib/src/presentation/riverpods/details_todo_notifier.dart
@@ -1,8 +1,11 @@
+import 'package:amuz_todo_list/src/domain/model/todo.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'details_todo_notifier.g.dart';
 
 @Riverpod(keepAlive: false)
 class DetailsTodoNotifier extends _$DetailsTodoNotifier {
-  void build() {}
+  Todo build() {
+    return Todo.empty();
+  }
 }
