120ddd6f3fad703150150dd0ac506bcdc2b83f89
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 17:45:05 +0900

    FEAT: 임시저장 테이블 추가

--- lib/src/data/data_sources/local_database.dart
+++ lib/src/data/data_sources/local_database.dart
@@ -1,5 +1,6 @@
 import 'package:amuz_todo_list/src/data/tables/images.dart';
 import 'package:amuz_todo_list/src/data/tables/tags.dart';
+import 'package:amuz_todo_list/src/data/tables/temp_todos.dart';
 import 'package:amuz_todo_list/src/data/tables/todos.dart';
 import 'package:amuz_todo_list/src/data/tables/todos_and_tags.dart';
 import 'package:drift/drift.dart';
@@ -8,7 +9,7 @@ import 'package:path_provider/path_provider.dart';
 
 part 'local_database.g.dart';
 
-@DriftDatabase(tables: [Todos, Tags, TodosAndTags, Images])
+@DriftDatabase(tables: [Todos, Tags, TodosAndTags, Images, TempTodos])
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
 
