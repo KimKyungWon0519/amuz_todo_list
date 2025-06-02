aa13f62790fb4c9da2e6bf9c9905e0f66bed5848
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 15:04:37 +0900

    FEAT: Override 문구 추가

--- lib/src/presentation/riverpods/details_todo_notifier.dart
+++ lib/src/presentation/riverpods/details_todo_notifier.dart
@@ -5,7 +5,12 @@ part 'details_todo_notifier.g.dart';
 
 @Riverpod(keepAlive: false)
 class DetailsTodoNotifier extends _$DetailsTodoNotifier {
+  @override
   Todo build() {
     return Todo.empty();
+  }
+
+  void updateTodo(Todo todo) {
+    state = todo;
   }
 }
