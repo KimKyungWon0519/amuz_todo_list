8cfede197450e95c5bacac241136741ec7c835f6
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 21:21:12 +0900

    FEAT: Fliter 버튼에 모두/완료/미완료 추가

--- lib/src/presentation/pages/todo_list_page/local_widgets/custom_appbar.dart
+++ lib/src/presentation/pages/todo_list_page/local_widgets/custom_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppbar({super.key});
@@ -8,7 +9,7 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       title: Text("ToDo"),
       actions: [
-        IconButton(onPressed: null, icon: Icon(Icons.filter_alt_rounded)),
+        _FilterButton(),
         IconButton(onPressed: null, icon: Icon(Icons.search_rounded)),
       ],
     );
@@ -16,4 +17,45 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+enum _FilterType {
+  all('전체'),
+  incomplete('미완료'),
+  complete('완료');
+
+  final String name;
+  const _FilterType(this.name);
+}
+
+class _FilterButton extends HookWidget {
+  const _FilterButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ValueNotifier<_FilterType> filterType = useState<_FilterType>(
+      _FilterType.all,
+    );
+
+    return PopupMenuButton(
+      itemBuilder:
+          (_) =>
+              _FilterType.values.map((type) {
+                return PopupMenuItem<_FilterType>(
+                  onTap: () {
+                    filterType.value = type;
+                  },
+                  value: type,
+                  child: Row(
+                    children: [
+                      Text(type.name),
+                      Spacer(),
+                      if (filterType.value == type) Icon(Icons.check),
+                    ],
+                  ),
+                );
+              }).toList(),
+      icon: Icon(Icons.filter_alt_rounded),
+    );
+  }
 }
