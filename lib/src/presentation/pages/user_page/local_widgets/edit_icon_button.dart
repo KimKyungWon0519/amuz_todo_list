d753d0541a3fa0dfc3be49993975358f8ee0113c
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 22:40:12 +0900

    FEAT: User Image 및 name 변경

--- lib/src/presentation/pages/user_page/local_widgets/edit_icon_button.dart
+++ lib/src/presentation/pages/user_page/local_widgets/edit_icon_button.dart
@@ -1,4 +1,8 @@
+import 'package:amuz_todo_list/src/presentation/pages/add_image_panel/add_image_panel.dart';
+import 'package:amuz_todo_list/src/presentation/pages/user_page/local_widgets/add_user_name_dialog.dart';
+import 'package:amuz_todo_list/src/presentation/riverpods/user_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class EditIconButton extends StatelessWidget {
   const EditIconButton({super.key});
@@ -20,17 +24,35 @@ class EditIconButton extends StatelessWidget {
   }
 }
 
-class _BottomSheet extends StatelessWidget {
+class _BottomSheet extends ConsumerWidget {
   const _BottomSheet({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return SafeArea(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          ListTile(title: Text("프로필 사진 변경")),
-          ListTile(title: Text("이름 변경")),
+          ListTile(
+            title: Text("프로필 사진 변경"),
+            onTap: () async {
+              String? imagePath = await showAddImageBottomSheet(context);
+
+              if (imagePath == null) return;
+
+              ref.read(userNotifierProvider.notifier).setImageUrl(imagePath);
+            },
+          ),
+          ListTile(
+            title: Text("이름 변경"),
+            onTap: () async {
+              String? name = await showAddUserNameDialog(context);
+
+              if (name == null || name.isEmpty) return;
+
+              ref.read(userNotifierProvider.notifier).setUserName(name);
+            },
+          ),
         ],
       ),
     );
--- lib/src/presentation/pages/user_page/user_page.dart
+++ lib/src/presentation/pages/user_page/user_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:amuz_todo_list/src/presentation/riverpods/user_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'local_widgets/custom_appbar.dart';
 
@@ -12,15 +16,31 @@ class UserPage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.account_circle,
-              size: MediaQuery.sizeOf(context).width / 3,
-            ),
-            Text('이름', style: TextTheme.of(context).titleLarge),
-          ],
+        child: Consumer(
+          builder: (context, ref, child) {
+            final UserState userState = ref.watch(userNotifierProvider);
+
+            return Column(
+              spacing: 8,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (userState.imageUrl != null)
+                  CircleAvatar(
+                    radius: MediaQuery.sizeOf(context).width / 6,
+                    backgroundImage: FileImage(File(userState.imageUrl!)),
+                  )
+                else
+                  Icon(
+                    Icons.account_circle,
+                    size: MediaQuery.sizeOf(context).width / 3,
+                  ),
+                Text(
+                  userState.userName ?? '이름 없음',
+                  style: TextTheme.of(context).titleLarge,
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
