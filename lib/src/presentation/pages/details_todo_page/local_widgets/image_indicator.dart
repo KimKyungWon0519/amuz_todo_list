f1437be0907e7053aa8e1290dfdc49a7a90e41e8
KimKyungWon0519 <gwon080870@gamil.com>
2025-06-02 15:49:31 +0900

    FEAT: 이미지 적용

--- lib/src/presentation/pages/details_todo_page/local_widgets/image_indicator.dart
+++ lib/src/presentation/pages/details_todo_page/local_widgets/image_indicator.dart
@@ -1,14 +1,17 @@
+import 'package:amuz_todo_list/src/presentation/riverpods/details_todo_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ImageIndicator extends HookWidget {
+class ImageIndicator extends HookConsumerWidget {
   final PageController? controller;
 
   const ImageIndicator({super.key, this.controller});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final ValueNotifier<int> currentIndex = useState(0);
+    final int imageLength = ref.watch(detailsTodoNotifierProvider.select((value) => value.images.length));
 
     useEffect(() {
       void updateCurrentIndex() {
@@ -30,7 +33,7 @@ class ImageIndicator extends HookWidget {
       ),
       padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       child: Text(
-        "${currentIndex.value + 1} / 5",
+        "${currentIndex.value + 1} / $imageLength",
         style: TextStyle(color: Colors.white),
       ),
     );
--- lib/src/presentation/pages/details_todo_page/local_widgets/image_pageview.dart
+++ lib/src/presentation/pages/details_todo_page/local_widgets/image_pageview.dart
@@ -1,22 +1,29 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ImagePageView extends StatelessWidget {
+import 'package:amuz_todo_list/src/presentation/riverpods/details_todo_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:amuz_todo_list/src/domain/model/image.dart' as Domain;
+
+class ImagePageView extends ConsumerWidget {
   final PageController? controller;
 
   const ImagePageView({super.key, this.controller});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Domain.Image> images = ref.watch(
+      detailsTodoNotifierProvider.select((value) => value.images),
+    );
+
     return PageView.builder(
       controller: controller,
       itemBuilder:
           (context, index) => Padding(
             padding: EdgeInsets.symmetric(horizontal: 8),
-            child: Image.network(
-              "https://storage.googleapis.com/cms-storage-bucket/c823e53b3a1a7b0d36a9.png",
-            ),
+            child: Image.file(File(images[index].url)),
           ),
-      itemCount: 5,
+      itemCount: images.length,
       scrollDirection: Axis.horizontal,
     );
   }
